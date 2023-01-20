d5e214cb576dd0c35a578dc603dee2f6328f535b
ghostadaz <122482879+ghostadaz@users.noreply.github.com>
2023-01-20 02:42:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const Telegraf = require('telegraf')
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const app = express()
+app.use(bodyParser.json())
 
-bot.start((ctx) => {
-  ctx.reply('Hello!')
+app.post('/', (req, res) => {
+  const { message } = req.body
+
+  // Handle the message here
+  // You can use the message text and sender information
+  // to perform different actions or send messages
+
+  // For example, send a reply to the message
+  const reply = `Merci pour votre message : ${message.text}`
+  sendMessage(reply, message.chat.id)
+
+  res.sendStatus(200)
 })
 
-bot.launch()
+// Function to send a message to Telegram
+const sendMessage = (text, chatId) => {
+  axios.post(
+    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+    {
+      chat_id: chatId,
+      text
+    }
+  )
+}
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${server.address().port}`)
+})

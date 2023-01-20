a0ded893e34f98cca67e9a59f47893ae00f3d0b2
ghostadaz <122482879+ghostadaz@users.noreply.github.com>
2023-01-20 02:34:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.5830036654:AAFPDip9UuBDEq5n-C4VGpWcBEO5dnFfmfs)
 
 bot.start((ctx) => {
   ctx.reply('Hello!')
